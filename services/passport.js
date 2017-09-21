acfeeffe6efe8ab07b05c8ff4e8fe38277cad11e
Trancever <dawidu@onet.pl>
2017-09-21 17:55:02 +0200

    Added mongoDB connection and first model.

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
+
+const User = mongoose.model('users')
 
 passport.use(
   new GoogleStrategy({
@@ -8,8 +11,18 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
   }, (accessToken, refreshToken, profile, done) => {
-    console.log('accessToken', accessToken)
-    console.log('refreshToken', refreshToken)
-    console.log('profile', profile)
+    User.findOne({ googleId: profile.id })
+      .then(existingUser => {
+        if (existingUser) {
+          // User found
+          done(null, existingUser)
+        } else {
+          // User not found, create one
+          new User({ googleId: profile.id })
+            .save()
+            .then(newUser => done(null, newUser))
+        }
+      })
+      .catch(error => console.log(error))
   })
 )
